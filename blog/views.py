5b17bab918f2bd76274e2b29595cc3536de85fc3
lxb1226 <1944303766@qq.com>
2020-04-28 14:18:53 +0800

    12-bootstrapcss美化

--- blog/views.py
+++ blog/views.py
@@ -8,6 +8,7 @@ from .models import Blog, BlogType
 def blog_list(request):
     context = {}
     context['blogs'] = Blog.objects.all()
+    context['blog_types'] = BlogType.objects.all()
     context['blogs_count'] = Blog.objects.all().count()
     return render_to_response('blog/blog_list.html', context)
 
@@ -23,4 +24,5 @@ def blogs_with_type(request, blog_type_pk):
     blog_type = get_object_or_404(BlogType, pk=blog_type_pk)
     context['blogs'] = Blog.objects.filter(blog_type=blog_type)
     context['blog_type'] = blog_type
+    context['blog_types'] = BlogType.objects.all()
     return render_to_response('blog/blogs_with_type.html', context)
