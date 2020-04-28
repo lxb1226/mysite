3d761239bfbae02cf23aec0726db10116d27b2e4
lxb1226 <1944303766@qq.com>
2020-04-28 16:31:23 +0800

    上下篇博客和按月分类

--- blog/views.py
+++ blog/views.py
@@ -6,11 +6,8 @@ from .models import Blog, BlogType
 
 # Create your views here.
 
-
-# 返回全部博客
-def blog_list(request):
+def get_blog_list_common_data(request, blogs_all_list):
     page_num = request.GET.get('page', 1)  # 获取url的页面参数
-    blogs_all_list = Blog.objects.all()
     paginator = Paginator(blogs_all_list, settings.EACH_PAGE_BLOGS_NUMBER)  # 每2篇进行分页
     page_of_blogs = paginator.get_page(page_num)
     current_page_num = page_of_blogs.number  # 获取当前页码
@@ -34,43 +31,38 @@ def blog_list(request):
     context['page_of_blogs'] = page_of_blogs
     context['blog_types'] = BlogType.objects.all()
     context['page_range'] = page_range
+    context['blog_dates'] = Blog.objects.dates('created_time', 'month', order='DESC')
+    return context
+
+
+# 返回全部博客
+def blog_list(request):
+    blogs_all_list = Blog.objects.all()
+    context = get_blog_list_common_data(request, blogs_all_list)
     return render_to_response('blog/blog_list.html', context)
 
 
 def blog_detail(request, blog_pk):
     context = {}
-    context['blog'] = get_object_or_404(Blog, pk=blog_pk)
+    blog = get_object_or_404(Blog, pk=blog_pk)
+    context['blog'] = blog
+    context['previous_blog'] = Blog.objects.filter(created_time__gt=blog.created_time).last()
+    context['next_blog'] = Blog.objects.filter(created_time__lt=blog.created_time).first()
     return render_to_response('blog/blog_detail.html', context)
 
 
 def blogs_with_type(request, blog_type_pk):
-    context = {}
-    page_num = request.GET.get('page', 1)  # 获取url的页面参数
     blog_type = get_object_or_404(BlogType, pk=blog_type_pk)
     blogs_all_list = Blog.objects.filter(blog_type=blog_type)
-    paginator = Paginator(blogs_all_list, settings.EACH_PAGE_BLOGS_NUMBER)  # 每2篇进行分页
-    page_of_blogs = paginator.get_page(page_num)
-    current_page_num = page_of_blogs.number  # 获取当前页码
-
-    # 获取当前页码前后2页的页码范围
-    page_range = list(range(max(current_page_num - 2, 1), current_page_num)) + \
-                 list(range(current_page_num, min(current_page_num + 2, paginator.num_pages) + 1))
-    # page_range = [current_page_num + i for i in range(-3, 3) if 0 < current_page_num + i <= paginator.num_pages]
-    # 加上省略页码标记
-    if page_range[0] - 1 >= 2:
-        page_range.insert(0, '...')
-    if paginator.num_pages - page_range[-1] >= 2:
-        page_range.append('...')
-    # 加上首页和尾页
-    if page_range[0] != 1:
-        page_range.insert(0, 1)
-    if page_range[-1] != paginator.num_pages:
-        page_range.append(paginator.num_pages)
-
-    context['blogs'] = page_of_blogs.object_list
-    context['page_of_blogs'] = page_of_blogs
-    context['blog_types'] = BlogType.objects.all()
-    context['page_range'] = page_range
+    context = get_blog_list_common_data(request, blogs_all_list)
     context['blog_type'] = blog_type
 
     return render_to_response('blog/blogs_with_type.html', context)
+
+
+def blogs_with_date(request, year, month):
+    blogs_all_list = Blog.objects.filter(created_time__year=year, created_time__month=month)
+    context = get_blog_list_common_data(request, blogs_all_list)
+    context['blogs_with_date'] = '%s年%s月' % (year, month)
+
+    return render_to_response('blog/blogs_with_date.html', context)
